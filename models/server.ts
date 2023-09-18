35b9814596dab046cb76c4f2fc909ebd39bc3d76
Santino <santinobertola03@gmail.com>
2023-09-18 20:31:59 -0300

    fix-cors-error

--- models/server.ts
+++ models/server.ts
@@ -28,11 +28,12 @@ export class Server {
   }
 
   middlewares(): void {
-    this.app.use(
-      cors({
-        origin: "https://integrador-react-xi.vercel.app",
-      })
-    );
+    this.app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', 'https://integrador-react-xi.vercel.app');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      next();
+    });
     this.app.use(express.json());
   }
 
