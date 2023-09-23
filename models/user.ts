4e9bff5fa204b59f7711009da396d693eef7f232
Santino <santinobertola03@gmail.com>
2023-09-23 20:21:37 -0300

    login-correcion

--- models/user.ts
+++ models/user.ts
@@ -4,7 +4,6 @@ export interface IUser {
   nombre: string;
   email: string;
   password: string;
-  rol?: string;
   code?: string;
   verified?: boolean;
 }
@@ -33,8 +32,8 @@ const UserSchema = new Schema<IUser>({
 
 
 UserSchema.methods.toJSON = function(){
-    const { __v, password, _id, code, ...usuario } = this.toObject()
-    return usuario
+  const {__v, password, _id, code, ...usuario } = this.toObject()
+  return usuario
 }
 
 const Usuario: Model<IUser> = model<IUser>("Usuario", UserSchema)
